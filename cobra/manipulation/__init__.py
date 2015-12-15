fcf697931b079df34673126f0ec0077ee52ef4c9
Peter St. John <peterc.stjohn@gmail.com>
2015-12-15 11:44:07 -0700

    adding back modification imports

--- cobra/manipulation/__init__.py
+++ cobra/manipulation/__init__.py
@@ -2,5 +2,5 @@ from .delete import delete_model_genes, undelete_model_genes, remove_genes, \
     find_gene_knockout_reactions
 from .modify import initialize_growth_medium, convert_to_irreversible, \
     revert_to_reversible, escape_ID, canonical_form, \
-    get_compiled_gene_reaction_rules
+    get_compiled_gene_reaction_rules, get_growth_medium, knocked_out
 from .annotate import add_SBO
